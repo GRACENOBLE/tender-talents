6f89bcd3a7865a95ae97f1aa1661b3afb2878f05
GRACENOBLE <gracenoble72@gmail.com>
2024-04-09 21:14:19 +0300

    changed name in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./Components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        
-      },
+      backgroundImage: {},
     },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
 export default config;
