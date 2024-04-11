17acdfd4cc3f8ff066ab763078d283ebd6059773
GRACENOBLE <gracenoble72@gmail.com>
2024-04-11 20:18:47 +0300

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const inter = Nunito_Sans({ subsets: ["latin"] });
 
