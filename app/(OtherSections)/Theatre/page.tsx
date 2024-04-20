c45e67d705040c4063dcb5d3128fd4d82d55ad74
xapis-21 <xapisofficial@gmail.com>
2024-04-20 12:21:23 +0300

    removd theatre page

--- app/(OtherSections)/Theatre/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1>THEATRE</h1>
-    </div>
-  );
-}
